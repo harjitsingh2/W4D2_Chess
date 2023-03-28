1b671f8474648a801fc964fcc5c6afdff8e9366a
harjitsingh3 <singh.harjit09@gmail.com>
2023-03-28 17:26:22 -0400

    Added pseudocode

--- Chess/Slideable.rb
+++ Chess/Slideable.rb
@@ -1,3 +1,9 @@
 module Slideable
-    
+    # rook. Fixed column alternating row. 
+    # or fixed row alternating column
+    HORIZONTAL_DIRS
+    # bishop. as piece moves, col & row increment by same amoun
+    DIAGONAL_DIRS 
+
+    # queen takes both of these properties
 end
--- Chess/Stepable.rb
+++ Chess/Stepable.rb
@@ -1,3 +1,3 @@
 module Stepable.rb
-    
+
 end
