547b780785b42346934c4e725f44d67f56ec6ad4
lewiskk016 <lewiskk016@gmail.com>
2023-03-28 14:11:52 -0400

    initializing board

--- practice-for-ch-ruby-chess-long-practice-main/Board.rb
+++ practice-for-ch-ruby-chess-long-practice-main/Board.rb
@@ -1,5 +1,11 @@
 class Board
-        
+
+    def initialize
+        @rows = Array.new(8) { Array.new(8)}
+        @rows[0..1] = Piece.new
+        @rows[6..7] = Piece.new
+        @rows[2..5] = NullPiece.new
+    end
 
 
 
