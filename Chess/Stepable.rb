40b165c8e132072cb528c83072aed99b2e9120c4
harjitsingh3 <singh.harjit09@gmail.com>
2023-03-29 15:28:52 -0400

    Update Stepable

--- Chess/Stepable.rb
+++ Chess/Stepable.rb
@@ -1,7 +1,6 @@
 
 module Stepable
 
-# fix iteration
     def moves
         x, y = self.pos
         new_pos = []
@@ -11,8 +10,7 @@ module Stepable
             new_row = row + x
             new_col = col + y
             # Check to see if new position
-            # is on board, empty or contains
-            # another piece of other color.
+            # is on board, empty or contains another piece of other color.
             if self.on_board?(new_row, new_col) && self.available?(new_row, new_col)
                 new_pos << [new_row, new_col]
             end
@@ -20,34 +18,19 @@ module Stepable
         new_pos
     end
 
+    # Check if new position is on board
     def on_board?(x, y)
         (x >= 0 && x <= 7) && (y >= 0 && y <= 7)
     end
 
+    # Check if there is a piece at new position
     def available?(x, y)
-
-        # On board at this position, the color
-        #  cannot be equal to color of self
-        
-        p @board[[x, y]]
-        @board[[x, y]] == nil || @board[[x, y]].color != self.color
+        @board[[x, y]].color == nil || @board[[x, y]].color != self.color
     end
 
     def move_diffs
         raise NotImplementedError
     end
-
-    #  # Check what is at a position
-    #  def [](pos)
-    #     row, col = pos
-    #     @grid[row][col]
-    # end
-
-    # # Set a value to a position
-    # def []=(pos, val)
-    #     row, col = pos
-    #     @grid[row][col] = val
-    # end
 
 
 end
