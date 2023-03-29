c6f647ac64805b054e40e2d0e8c06ce3fc6240da
harjitsingh3 <singh.harjit09@gmail.com>
2023-03-29 15:17:29 -0400

    Populate board

--- Chess/Board.rb
+++ Chess/Board.rb
@@ -1,8 +1,15 @@
 require_relative 'Piece.rb'
 require_relative 'NullPiece.rb'
+require_relative 'Rook.rb'
+require_relative 'Knight.rb'
+require_relative 'Bishop.rb'
+require_relative 'Queen.rb'
+require_relative 'King.rb'
+require_relative 'Pawn.rb'
+
 class Board
 
-    attr_reader :grid
+    attr_accessor :grid
 
     def initialize
         @grid= Array.new(8) { Array.new(8, nil)}
@@ -40,19 +47,45 @@ class Board
 
     # private
     def populate
-        # @grid[0][0] = Rook.new()
-
-        (0...2).each do |i|
+        # White Side
+        @grid[0][0] = Rook.new(:white, self, [0,0])
+        @grid[0][1] = Knight.new(:white, self, [0,1])
+        @grid[0][2] = Bishop.new(:white, self, [0,2])
+        @grid[0][3] = Queen.new(:white, self, [0,3])
+        @grid[0][4] = King.new(:white, self, [0,4])
+        @grid[0][5] = Bishop.new(:white, self, [0,5])
+        @grid[0][6] = Knight.new(:white, self, [0,6])
+        @grid[0][7] = Rook.new(:white, self, [0,7])
+        #Pawns
+        (1...2).each do |i|
             (0...grid.length).each do |j|
-                    grid[i][j] = Piece.new
+                grid[i][j] = Pawn.new(:white, self, [i,j])
+            end
+        end
+        
+        # Black Side
+        @grid[7][0] = Rook.new(:black, self, [7,0])
+        @grid[7][1] = Knight.new(:black, self, [7,1])
+        @grid[7][2] = Bishop.new(:black, self, [7,2])
+        @grid[7][3] = Queen.new(:black, self, [7,3])
+        @grid[7][4] = King.new(:black, self, [7,4])
+        @grid[7][5] = Bishop.new(:black, self, [7,5])
+        @grid[7][6] = Knight.new(:black, self, [7,6])
+        @grid[7][7] = Rook.new(:black, self, [7,7])
+        #Pawns
+        (6...7).each do |i|
+            (0...grid.length).each do |j|
+                grid[i][j] = Pawn.new(:black, self, [i,j])
             end
         end
 
-        (6..7).each do |i|
+        # Null Pieces
+        (2..5).each do |i|
             (0...grid.length).each do |j|
-                    grid[i][j] = Piece.new
+                grid[i][j] = NullPiece.new(nil, self, [i,j])
             end
         end
+
     end
 
 
--- Chess/NullPiece.rb
+++ Chess/NullPiece.rb
@@ -1,3 +1,5 @@
+require_relative "Piece"
+
 class NullPiece < Piece
 
 
