0da9c9926ad1492ff90df1bbbe6e9ef71f563890
lewiskk016 <lewiskk016@gmail.com>
2023-03-29 15:41:15 -0400

    finished knight

--- Chess/Knight.rb
+++ Chess/Knight.rb
@@ -4,4 +4,13 @@ require_relative "Stepable"
 class Knight < Piece
     include Stepable
 
+    def symbol
+        '♞'.colorize(color)
+    end
+
+    def move_diffs
+        possible = [[1,2], [1,-2], [2,1], [2,-1], [-2,1], [-2,-1], [-1,2], [-1,-2]]
+        return possible
+    end
+
 end
